4be72f5a53ec6552063e0cc7da2a5e6f04b93423
fpxzzb <chen7210517@gmail.com>
2025-07-13 20:52:35 +0800

    build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -40,7 +40,6 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
-PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
@@ -49,26 +48,7 @@ PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
-PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-# 文件管理器
-PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
-# 静态文件服务器dufs(推荐)
-PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
-
-# ============= imm仓库外的第三方插件==============
-# ============= 若启用 则打开注释 ================
-# istore商店
-#PACKAGES="$PACKAGES luci-app-store"
-# 首页和网络向导
-#PACKAGES="$PACKAGES luci-i18n-quickstart-zh-cn"
-# 去广告adghome
-#PACKAGES="$PACKAGES luci-app-adguardhome"
-# 代理相关
-#PACKAGES="$PACKAGES luci-app-ssr-plus"
-#PACKAGES="$PACKAGES luci-app-passwall2"
-#PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
 # VPN
 #PACKAGES="$PACKAGES luci-app-tailscale"
 #PACKAGES="$PACKAGES luci-i18n-tailscale-zh-cn"
